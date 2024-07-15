170f99f33449a9fe2452eac1a3ff20df22325384
Andrey <sawcrazy@yandex.ru>
2024-07-15 16:00:26 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+export const App = ()=> {
+
+
+  return (
+    <>
+        <div className="container">
+            <header>
+                <div className="logo">
+
+                </div>
+                <nav className="menu">
+                    <ul>
+                        <li>О сервисе</li>
+                        <li>Возможности</li>
+                        <li>Тарифы</li>
+                        <li>О Нас</li>
+                        <li>Приложения</li>
+                    </ul>
+                </nav>
+                <div>
+                    <button>
+                        push me
+                    </button>
+                </div>
+            </header>
+        </div>
+    </>
+  )
+}
