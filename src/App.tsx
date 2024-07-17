f3087ad86e919976ecded2be86758642a01fa254
Andrey <sawcrazy@yandex.ru>
2024-07-17 16:29:40 +0300

    section1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Logo from '/logo.png';
-import Vector from '/vector.svg';
+import Logo from './img/logo.svg';
+import Video from './img/video.png'
+import Vector from "./img/vector.svg?react";
 import './App.css'
 export const App = ()=> {
-
-
   return (
     <>
-        <div className="container">
             <header className="header">
                 <a className="header__logo"><img src={Logo} alt="Logo"/></a>
                 <nav className="header__menu">
@@ -21,10 +19,31 @@ export const App = ()=> {
                 <div >
                     <button className="header__button">
                         <p>Запустить редактор</p>
-                        <img src={Vector} alt="vector"/>
+                        <Vector/>
                     </button>
                 </div>
             </header>
+        <div className="container">
+            <section className="possibilities">
+                <div className="possibilities__text">
+                    <div className="possibilities__title">
+                        <h1>Визуализируйте и объясняйте лучше, используя интерактивное 3D и AR</h1>
+                    </div>
+                    <p>Встречайте Vectary, онлайн-платформу для создания и
+                        управления интерактивными
+                        <br/>
+                        3D- и AR-проектами,
+                        которые вдохновляют, обучают и решают проблемы.
+                        <br/>
+                        ‍Никаких загрузок, никакого кода: все в браузере.
+                    </p>
+                </div>
+                <button className="possibilities__button">
+                    <p>Попробовать бесплатно</p>
+                    <Vector/>
+                </button>
+                <img src={Video} alt="Video" className="possibilities__img"/>
+            </section>
         </div>
     </>
   )
