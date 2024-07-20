239e01ca306f6757bf69afeaacc563cf83cdaa76
Andrey <sawcrazy@yandex.ru>
2024-07-20 16:20:23 +0300

    section_for

--- src/сomponents/carts/cart.tsx
+++ src/сomponents/carts/cart.tsx
@@ -1,18 +1,19 @@
 import {ButtonWhite} from "../buttons/button_white";
+import s from './style.module.css';
 
 export const Cart = (props) =>{
     return(
-        <>
-            <div className="cart">
-                <div>
-                    <img src={props.img} alt=""/>
+            <div className={s.cart}>
+                <div className={s.cart__img}>
+                    <img src={props.img} alt="img"/>
                 </div>
-                <div className="cart__name">
+                <div className={s.cart__name}>
                     <h4>{props.name}</h4>
+                </div>
+                <div className={s.cart__button}>
                     <ButtonWhite name="Посмотреть демо"/>
                 </div>
             </div>
-        </>
     )
 }
 
